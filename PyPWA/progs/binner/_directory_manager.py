8859b28ba2c4557ba704605a0feaa702b075b79a
Mark Jones <maj@jlab.org>
2018-06-04 14:24:57 -0400

    Fix binning and sorting, removes threading
    This fixes the issue with bins not going to the correct bin, as well as
    removes threads and processing, threads never improved the write speed
    unfortunately

---
+++ PyPWA/progs/binner/_directory_manager.py
@@ -0,0 +1,167 @@
+#  coding=utf-8
+#
+#  PyPWA, a scientific analysis toolkit.
+#  Copyright (C) 2016 JLab
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+"""
+from typing import Any, Dict, List
+
+from PyPWA import Path, AUTHOR, VERSION
+from PyPWA.libs.components.data_processor import (
+    file_processor, data_templates
+)
+from PyPWA.progs.binner import _bin_data
+
+__credits__ = ["Mark Jones"]
+__author__ = AUTHOR
+__version__ = VERSION
+
+
+class _GetFileHandle(object):
+
+    def __init__(self):
+        # type: () -> None
+        self.__parser = file_processor.DataProcessor()
+
+    def get_file_handle(self, source, extras, destination):
+        # type: (Path, List[Path], Path) -> Dict[str, Any]
+        self.__create_file_tree(destination)
+        return {
+            'destination': self.__process_single_file(source, destination),
+            'extras': self.__process_multiple_files(extras, destination)
+        }
+
+    @staticmethod
+    def __create_file_tree(destination):
+        # type: (Path) -> None
+        destination.mkdir(parents=True, exist_ok=True)
+
+    def __process_single_file(self, file, destination):
+        # type: (Path, Path) -> data_templates.Writer
+        return self.__parser.get_writer(
+            destination / file.name, is_particle_pool=True
+        )
+
+    def __process_multiple_files(self, files, destination):
+        # type: (List[Path], Path) -> List[data_templates.Writer]
+        handler_list = []
+        for file in files:
+            handler_list.append(self.__process_single_file(file, destination))
+        return handler_list
+
+
+class _FileHandles(object):
+
+    def __init__(self):
+        self.__get_file_handles = _GetFileHandle()
+
+    def get_bin_handles(self, source_file, extras, root, bin_types):
+        file_handles = dict()
+        for lower_limit in bin_types[0].lower_limits_list:
+            destination = self.__get_destination(root, lower_limit, bin_types)
+            if len(bin_types[1:]):
+                file_handles[lower_limit] = self.get_bin_handles(
+                    source_file, extras, destination, bin_types[1:]
+                )
+            else:
+                file_handles[lower_limit] = self.__produce_file_handles(
+                    source_file, extras, destination
+                )
+        return file_handles
+
+    @staticmethod
+    def __get_destination(root, lower_limit, bin_types):
+        # type: (Path, int, List[_bin_data.BinSettings]) -> Path
+        prefix = bin_types[0].get_calculation_prefix()
+        destination = root / (str(lower_limit) + prefix)
+        return destination
+
+    def __produce_file_handles(self, source, extras, destination):
+        return self.__get_file_handles.get_file_handle(
+            source, extras, destination
+        )
+
+
+class _CloseHandles(object):
+
+    def close_handles(self, directory_dictionary):
+        # type: (dict) -> None
+        for value in directory_dictionary.values():
+            if 'destination' in value:
+                self.__close_handles(value)
+            else:
+                self.close_handles(value)
+
+    @staticmethod
+    def __close_handles(value):
+        value['destination'].close()
+        for extra in value['extras']:
+            extra.close()
+
+
+class BinDirectoryManager(object):
+
+    def __init__(self, settings):
+        # type: (_bin_data.SettingsCollection) -> None
+        self.__handles = None
+        self.__opened = False
+        self.__settings = settings
+        self.__handle_closer = _CloseHandles()
+        self.__handle_opener = _FileHandles()
+
+    def open(self):
+        if not self.__opened:
+            self.__handles = self.__handle_opener.get_bin_handles(
+                self.__settings.file_settings.source_file,
+                self.__settings.file_settings.extra_files,
+                Path(), self.__settings.bin_settings
+            )
+            self.__opened = True
+        else:
+            raise RuntimeError("Directory Manager is already open!")
+
+    def __enter__(self):
+        self.open()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def close(self):
+        if self.__opened:
+            self.__handle_closer.close_handles(self.__handles)
+            self.__opened = False
+        else:
+            raise RuntimeError("Directory Manager isn't open!")
+
+    def write(self, write_packet):
+        writers = self.__get__writers(write_packet['bins'])
+        self.__write_packet(writers, write_packet['files'])
+
+    def __get__writers(self, bins):
+        sample = self.__handles
+        for index_bin in bins:
+            sample = sample[index_bin]
+        return sample
+
+    @staticmethod
+    def __write_packet(writers, files):
+        # type: (dict, dict) -> None
+        writers['destination'].write(files['destination'])
+        for packet in zip(writers['extras'], files['extras']):
+            packet[0].write(packet[1])
--- PyPWA/progs/binner/binning.py
+++ PyPWA/progs/binner/binning.py
@@ -19,82 +19,125 @@
 """
 
 """
-
-import enum
-from typing import List
 import numpy
+import tqdm
 
-from PyPWA import Path, AUTHOR, VERSION
+from PyPWA import queue, AUTHOR, VERSION
+from PyPWA.libs.components.data_processor import (
+    file_processor, data_templates
+)
+from PyPWA.libs.math import vectors, particle
+from PyPWA.progs.binner import _bin_data, _directory_manager
 
 __credits__ = ["Mark Jones"]
 __author__ = AUTHOR
 __version__ = VERSION
 
 
-class BinErrors(enum.Enum):
-    Error = enum.auto
+class _MultipleReader(object):
 
+    def __init__(self, file_settings):
+        # type: (_bin_data.FileSettings) -> None
+        self.__event_count = None  # type: int
+        self.__processor = file_processor.DataProcessor()
+        self.__source = self.__setup_source(file_settings)
+        self.__extras = self.__setup_extras(file_settings)
 
-class DataSort(object):
+    def __setup_source(self, file_settings):
+        main_reader = self.__processor.get_reader(file_settings.source_file)
+        self.__event_count = main_reader.get_event_count()
+        return main_reader
 
-    def __init__(self, directory, postfix, bin_edges, bin_count):
-        # type: (Path, List[int, int], int) -> None
-        self.__bins = self.__calculate_bins(sorted(bin_edges), bin_count)
-        self.__tracker = self.__prime_tracker(directory, postfix, self.__bins)
+    def __setup_extras(self, file_settings):
+        extras = []
+        for file in file_settings.extra_files:
+            reader = self.__processor.get_reader(file)
+            self.__check_event_length(reader)
+            extras.append(reader)
+        return extras
 
-    @staticmethod
-    def __calculate_bins(bin_edges, bin_count):
-        # type: (List[int, int], int) -> List[int]
-        bin_range = numpy.ptp(bin_edges)
-        bin_size = int(bin_range / bin_count)
-        stop = int(bin_range[0]+bin_size*bin_count)
-        return list(range(bin_range[0], stop, bin_size))
+    def __check_event_length(self, reader):
+        # type: (data_templates.Reader) -> None
+        if reader.get_event_count() != self.__event_count:
+            raise RuntimeError("Input files have a different event count!")
 
-    @staticmethod
-    def __prime_tracker(directory, postfix, bins):
-        tracker = dict()
-        for the_bin in bins:
-            tracker[the_bin] = {
-                'directory': directory / str(the_bin) + postfix,
-                'writer': False
-            }
-        return tracker
+    def __len__(self):
+        return self.__event_count
 
-    def initialize(self):
-        self.__create_directories()
-        self.__open_file_handles()
+    def __iter__(self):
+        return self
 
-    def __create_directories(self):
-        for bin_info in self.__tracker.items():
-            bin_info[1]['directory'].mkdir(parents=True, exist_ok=True)
+    def __enter__(self):
+        return self
 
-    def __open_file_handles(self):
-        pass # For now...
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
 
-    def write_bin(self, bin_value, event):
-        bin_location = self.__get_bin_location(bin_value)
-        if bin_location == BinErrors.Error:
-            print("Skipping due to error!")
-            return
+    def __next__(self):
+        return self.next()
 
-        self.__tracker[bin_location]['writer'].write(event)
+    def next(self):
+        return {
+            'destination': self.__get_source(),
+            'extras': self.__get_extras()
+        }
 
-    def __get_bin_location(self, bin_value):
-        previous_bin = None
-        for index, current_bin in enumerate(self.__bins):
-            if not index:
-                previous_bin = current_bin
-                if current_bin > bin_value:
-                    print("Underflow!")
-                    return BinErrors.Error
+    def __get_source(self):
+        return self.__source.next()
 
-            if current_bin > bin_value > previous_bin:
-                return previous_bin
-
-        print("Overflow!")
-        return BinErrors.Error
+    def __get_extras(self):
+        extras = []
+        for extra in self.__extras:
+            extras.append(extra.next())
+        return extras
 
     def close(self):
-        for bin_info in self.__tracker.items():
-            bin_info[1]['writer'].close()
+        self.__source.close()
+        for file in self.__extras:
+            file.close()
 
+
+class Binning(object):
+
+    def __init__(self, settings_collection):
+        # type: (_bin_data.SettingsCollection) -> None
+        self.__setting = settings_collection
+
+    def bin(self):
+        with _MultipleReader(self.__setting.file_settings) as multiple_reader:
+            self.__iterate_over_events(multiple_reader)
+
+    def __iterate_over_events(self, multiple_reader):
+        args = {
+            "desc": "Binning", "unit": "events"
+        }
+        with _directory_manager.BinDirectoryManager(self.__setting) as write:
+            for event in tqdm.tqdm(multiple_reader, **args):
+                mass = self.__calculate_mass(event['destination'])
+                packet = {
+                    'bins': self.__find_bin(mass),
+                    'files': event
+                }
+                write.write(packet)
+
+    @staticmethod
+    def __calculate_mass(event):
+        array = numpy.zeros(1, particle.NUMPY_PARTICLE_DTYPE)
+        particle_vector = vectors.FourVector(array)
+        for the_particle in event.iterate_over_particles():
+            if the_particle.id not in (1, 14):
+                particle_vector += the_particle
+        return numpy.sqrt(particle_vector.get_dot(particle_vector))*1000
+
+    def __find_bin(self, calculated_value):
+        previous_bin = None
+        lower_limits = self.__setting.bin_settings[0].lower_limits_list
+        for index, current_bin in enumerate(lower_limits):
+            if not index:
+                previous_bin = current_bin
+                if current_bin > calculated_value:
+                    return [600]
+            if current_bin > calculated_value > previous_bin:
+                return [previous_bin]
+            previous_bin = current_bin
+        return [current_bin]
